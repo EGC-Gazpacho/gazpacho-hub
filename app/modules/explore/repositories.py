2be7e91f49876f2450dc6febf5e03fd28259f4c4
mohamedabouri <mohabo1@alum.us.es>
2024-11-05 11:39:50 +0100

    Filter by number of features done in frontend and some changes in the backend

--- app/modules/explore/repositories.py
+++ app/modules/explore/repositories.py
@@ -1,7 +1,7 @@
 import re
 from sqlalchemy import any_, or_
 import unidecode
-from app.modules.dataset.models import Author, DSMetaData, DataSet, PublicationType
+from app.modules.dataset.models import Author, DSMetaData, DataSet, PublicationType, DSMetrics
 from app.modules.featuremodel.models import FMMetaData, FeatureModel
 from core.repositories.BaseRepository import BaseRepository
 
@@ -52,8 +52,9 @@ class ExploreRepository(BaseRepository):
         if tags:
             datasets = datasets.filter(DSMetaData.tags.ilike(any_(f"%{tag}%" for tag in tags)))
 
-        if number_of_features is not None:
-            datasets = datasets.filter(DSMetaData.DSMetrics.number_of_features == number_of_features)
+        if number_of_features != "any":
+            datasets = datasets.join(DSMetaData.ds_metrics)
+            datasets = datasets.filter(DSMetrics.number_of_features == number_of_features)
 
         # Order by created_at
         if sorting == "oldest":
