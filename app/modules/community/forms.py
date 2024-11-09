214c47dc7d0170dcd6e51fae63c36de5fb150e82
RafaelCabello <rafcabran@alum.us.es>
2024-11-09 23:22:39 +0100

    feat: Private communities, community roles and community admin dashboard

--- app/modules/community/forms.py
+++ app/modules/community/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import SelectField, StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
+
+from app.modules.community.models import CommunityType
 
 
 class CommunityForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=100)])
     description = TextAreaField('Description', validators=[DataRequired()])
+    type = SelectField(
+        "Type",
+        choices=[(type.value, type.name.capitalize()) for type in CommunityType],
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Save Community')
--- app/modules/community/repositories.py
+++ app/modules/community/repositories.py
@@ -1,4 +1,4 @@
-from app.modules.community.models import Community, UserCommunity
+from app.modules.community.models import Community, JoinRequest, UserCommunity, UserRole
 from core.repositories.BaseRepository import BaseRepository
 from app import db
 
@@ -24,7 +24,7 @@ class UserCommunityRepository(BaseRepository):
     def get_user_community(self, user_id, community_id):
         return self.model.query.filter_by(user_id=user_id, community_id=community_id).first()
 
-    def add_user_to_community(self, user_id, community_id, role="Member"):
+    def add_user_to_community(self, user_id, community_id, role=UserRole.MEMBER):
         return self.create(user_id=user_id, community_id=community_id, role=role)
 
     def delete(self, user_id, community_id):
@@ -35,3 +35,20 @@ class UserCommunityRepository(BaseRepository):
 
     def delete_all_by_community_id(self, community_id):
         self.model.query.filter_by(community_id=community_id).delete()
+
+    def update_role(self, user_id, community_id, role):
+        association = self.model.query.filter_by(user_id=user_id, community_id=community_id).first()
+        if association:
+            association.role = role
+            db.session.commit()
+
+
+class JoinRequestRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(JoinRequest)
+
+    def get_request_by_id(self, request_id):
+        return self.model.query.filter_by(id=request_id).first()
+
+    def is_request_already_made(self, user_id, community_id):
+        return self.model.query.filter_by(user_id=user_id, community_id=community_id).first() is not None
