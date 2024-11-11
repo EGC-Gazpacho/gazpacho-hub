aaa4ae68feb40e9e6b77f6dcd7b97081372f45cf
Javier Rodriguez Reina <javrodrei@alum.us.es>
2024-11-11 14:45:45 +0100

    fix:Some lint errors

--- app/modules/dashboard/forms.py
+++ app/modules/dashboard/forms.py
@@ -2,5 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
+
 class DashboardForm(FlaskForm):
     ndatasets = StringField('Number', validators=[DataRequired()])
--- app/modules/dashboard/routes.py
+++ app/modules/dashboard/routes.py
@@ -11,6 +11,8 @@ datasetservice = DataSetService()
 '''
 READ ALL
 '''
+
+
 @dashboard_bp.route('/dashboard', methods=['GET'])
 @login_required
 def index():
@@ -18,14 +20,12 @@ def index():
     ndatasets = datasetservice.count_dsmetadata()
     nauthors = datasetservice.count_authors()
 
-    #author_names = ["Juan", "Pedro", "Maria", "Ana"]
-    #datasets_count = [2,3,4,7]
+    # author_names = ["Juan", "Pedro", "Maria", "Ana"]
+    # datasets_count = [2,3,4,7]
 
     author_names, dataset_counts = dashboardService.get_all_author_names_and_dataset_counts()
     print("Author Names:", author_names)
     print("Dataset Counts:", dataset_counts)
 
-    
-
-    return render_template('dashboard/index.html', ndatasets=ndatasets, nauthors=nauthors, 
+    return render_template('dashboard/index.html', ndatasets=ndatasets, nauthors=nauthors,
                            author_names=author_names, datasets_count=dataset_counts, form=form)
