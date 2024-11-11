258ec6373f7470b91bc385284f9755dece3a4b33
romerito3rubn <romerito3rubn@gmail.com>
2024-11-11 17:00:24 +0100

    feat: Remember my password module

--- app/modules/auth/services.py
+++ app/modules/auth/services.py
@@ -1,6 +1,10 @@
 import os
-from flask_login import login_user
-from flask_login import current_user
+import jwt
+from datetime import datetime, timedelta
+from flask import current_app
+from flask_login import login_user, current_user
+from flask_mail import Message
+from app import mail
 
 from app.modules.auth.models import User
 from app.modules.auth.repositories import UserRepository
@@ -64,8 +68,11 @@ class AuthenticationService(BaseService):
         if form.validate():
             updated_instance = self.update(user_profile_id, **form.data)
             return updated_instance, None
-
         return None, form.errors
+
+    def get_user_by_email(self, email):
+        """Obtiene un usuario basado en su correo electrónico."""
+        return self.repository.get_by_email(email)
 
     def get_authenticated_user(self) -> User | None:
         if current_user.is_authenticated:
@@ -79,3 +86,46 @@ class AuthenticationService(BaseService):
 
     def temp_folder_by_user(self, user: User) -> str:
         return os.path.join(uploads_folder_name(), "temp", str(user.id))
+
+    def generate_recovery_token(self, user):
+        """Genera un token de recuperación para el usuario"""
+        expiration = datetime.utcnow() + timedelta(hours=1)
+        token = jwt.encode({'user_id': user.id, 'exp': expiration}, current_app.config['SECRET_KEY'], algorithm='HS256')
+        return token
+
+    def verify_recovery_token(self, token):
+        """Verifica el token de recuperación"""
+        try:
+            data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_id = data.get('user_id')
+            return user_id
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+
+    def update_password(self, user_id, new_password):
+        """Actualiza la contrasena del usuario"""
+        user = self.repository.get_by_id(user_id)
+        if user:
+            user.set_password(new_password)
+            self.repository.session.commit()
+            return True
+        return False
+
+    def send_recovery_email(self, to, token):
+        """Envía un correo electrónico con el enlace de recuperacion de contrasena"""
+        subject = "Recuperacion de contraseña"
+        recovery_url = f"{current_app.config['BASE_URL']}/password_reset/{token}"
+        body = f"Por favor, haz clic en el siguiente enlace para restablecer tu contraseña: {recovery_url}"
+
+        msg = Message(subject=subject, recipients=[to])
+        msg.body = body  # No es necesario codificarlo a UTF-8, ya lo maneja flask_mail automáticamente
+        msg.charset = 'utf-8'
+        print("hola3", msg.body, msg.subject) 
+        try:
+            mail.send(msg)  # Enviar el correo
+            return True  # Correo enviado correctamente
+        except Exception as e:
+            current_app.logger.error(f"Error al enviar correo: {str(e)}")  # Registrar error
+            raise e  # Volver a lanzar el error para manejarlo en el controlador
