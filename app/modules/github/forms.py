eedc36a09d8175f368ef8e3913e37c927e2283e4
Rafael Duque <91947361+rafduqcol@users.noreply.github.com>
2024-11-11 11:04:55 +0100

    fix: Remove unused Optional

--- app/modules/github/forms.py
+++ app/modules/github/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired
 
 class DataSetFormGithub(FlaskForm):
     title = StringField('Dataset Title', validators=[DataRequired()])
