a246e52d405a23ee359092e904dcdeea5e57ad31
fracalrod3 <fracalrod3@alum.us.es>
2024-11-25 17:34:00 +0000

    feat: models.py

--- app/modules/dataset/models.py
+++ app/modules/dataset/models.py
@@ -61,6 +61,7 @@ class DSMetaData(db.Model):
     description = db.Column(db.Text, nullable=False)
     publication_type = db.Column(SQLAlchemyEnum(PublicationType), nullable=False)
     publication_doi = db.Column(db.String(120))
+    rating = db.Column(db.Float, default=0.0)
     dataset_doi = db.Column(db.String(120))
     tags = db.Column(db.String(120))
     ds_metrics_id = db.Column(db.Integer, db.ForeignKey('ds_metrics.id'))
@@ -107,6 +108,12 @@ class DataSet(db.Model):
     def get_uvlhub_doi(self):
         from app.modules.dataset.services import DataSetService
         return DataSetService().get_uvlhub_doi(self)
+    
+    def update_rating(self):
+        total_ratings = sum(rating.rating for rating in self.ratings)
+        count = len(self.ratings)
+        self.rating = total_ratings / count if count > 0 else 0
+        db.session.commit()
 
     def to_dict(self):
         return {
@@ -118,6 +125,7 @@ class DataSet(db.Model):
             'authors': [author.to_dict() for author in self.ds_meta_data.authors],
             'publication_type': self.get_cleaned_publication_type(),
             'publication_doi': self.ds_meta_data.publication_doi,
+            'rating': self.ds_meta_data.rating,
             'dataset_doi': self.ds_meta_data.dataset_doi,
             'tags': self.ds_meta_data.tags.split(",") if self.ds_meta_data.tags else [],
             'url': self.get_uvlhub_doi(),
@@ -164,3 +172,15 @@ class DOIMapping(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dataset_doi_old = db.Column(db.String(120))
     dataset_doi_new = db.Column(db.String(120))
+    
+
+class DSRating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    dataset_id = db.Column(db.Integer, db.ForeignKey('ds_meta_data.id'), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    rated_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    ds_meta_data = db.relationship('DSMetaData', backref=db.backref('ratings', lazy=True))
+
+
