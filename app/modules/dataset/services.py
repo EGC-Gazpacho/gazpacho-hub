9596c030a50d67a83a343100a6c1fb0f0f4e9e96
fracalrod3 <fracalrod3@alum.us.es>
2024-11-25 17:47:01 +0000

    feat: added services for rating datasets

--- app/modules/dataset/services.py
+++ app/modules/dataset/services.py
@@ -8,14 +8,15 @@ import uuid
 from flask import request
 
 from app.modules.auth.services import AuthenticationService
-from app.modules.dataset.models import DSViewRecord, DataSet, DSMetaData, DSMetrics
+from app.modules.dataset.models import DSViewRecord, DataSet, DSMetaData, DSMetrics, DSRating
 from app.modules.dataset.repositories import (
     AuthorRepository,
     DOIMappingRepository,
     DSDownloadRecordRepository,
     DSMetaDataRepository,
     DSViewRecordRepository,
-    DataSetRepository
+    DataSetRepository,
+    DSRatingRepository
 )
 from app.modules.featuremodel.repositories import FMMetaDataRepository, FeatureModelRepository
 from app.modules.hubfile.repositories import (
@@ -272,3 +273,23 @@ class SizeService():
             return f'{round(size / (1024 ** 2), 2)} MB'
         else:
             return f'{round(size / (1024 ** 3), 2)} GB'
+
+
+class DSRatingService(BaseService):
+    def __init__(self):
+        super().__init__(DSRatingRepository())
+
+    def add_or_update_rating(self, dataset_id: int, user_id: int, rating_value: int) -> DSRating:
+        rating = self.repository.get_user_rating(dataset_id, user_id)
+        if rating:
+            rating.value = rating_value
+        else:
+            rating = self.repository.create(commit=False, data_set_id=dataset_id, user_id=user_id, value=rating_value)
+        self.repository.session.commit()
+        return rating
+
+    def get_dataset_average_rating(self, dataset_id: int) -> float:
+        return self.repository.get_average_rating(dataset_id)
+
+    def get_total_ratings(self, dataset_id: int) -> int:
+        return self.repository.count_ratings(dataset_id)
