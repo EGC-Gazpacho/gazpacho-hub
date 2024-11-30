f40da8cd8fa576d1bc31f300053e01bdc561fb4c
Rafael Duque <rafduqcol@alum.us.es>
2024-11-30 20:01:50 +0100

    fix: Clean code and change how to get de name of the DataSet

--- app/modules/github/services.py
+++ app/modules/github/services.py
@@ -9,7 +9,6 @@ def upload_dataset_to_github(owner, repo_name, dataset, token, commit_message, l
     Si el repositorio no existe, lo crea primero.
     """
 
-    # Primero, verificamos si el repositorio existe
     if repo_type == 'new':
         repo_created = create_repo(owner, repo_name, token)
         if not repo_created:
@@ -17,42 +16,34 @@ def upload_dataset_to_github(owner, repo_name, dataset, token, commit_message, l
 
     github_api = f"https://api.github.com/repos/{owner}/{repo_name}/contents/"
 
-    # Usamos el nombre del dataset como el nombre de la carpeta raíz en GitHub
-    folder_name = dataset.name  # Nombre del dataset como nombre de la carpeta
-
-    # Crear una lista para los archivos que vamos a subir
+    dataset_name = dataset.name()  # Acceso correcto al atributo
     files_content = []
 
-    # Iterar sobre los archivos del dataset y prepararlos para la subida
     for fm in dataset.feature_models:
         for file in fm.files:
-            print(f"Subiendo archivo: {file.__dict__}")  # Esto imprimirá los atributos de 'file'
-
-            # Obtener el archivo binario y codificarlo en base64
-            file_path = file.get_path()  # Suponiendo que get_path() devuelve la ruta del archivo
+            print(f"Subiendo archivo: {file.__dict__}") 
+            file_path = file.get_path()  
 
             with open(file_path, "rb") as f:
                 encoded_content = base64.b64encode(f.read()).decode('utf-8')
 
-            # Preparar la información del archivo para GitHub
             file_data = {
-                'path': f"{folder_name}/{file.name}",  # Ruta del archivo dentro de la carpeta
-                'content': encoded_content,  # Contenido del archivo codificado en base64
+                'path': f"{dataset_name}/{file.name}",  
+                'content': encoded_content,  
             }
             files_content.append(file_data)
 
-    # Subir los archivos a GitHub
     headers = {
         "Authorization": f"token {token}",
         "Content-Type": "application/json"
     }
 
-    # Subir cada archivo
     for file_data in files_content:
         data = {
             "message": commit_message,
-            "content": file_data['content'],  # Contenido codificado en base64
+            "content": file_data['content'],  
             "branch": "main",
+            "licene": license
         }
 
         github_api_with_file = f"{github_api}{file_data['path']}"
@@ -78,7 +69,7 @@ def create_repo(owner, repo_name, token):
 
     data = {
         "name": repo_name,
-        "private": False,  # Cambia a True si quieres que el repositorio sea privado
+        "private": False,  
         "description": "Repositorio creado a través de la API"
     }
 
