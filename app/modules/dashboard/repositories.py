2e3fbe9a37d64e84e8bcb75350344b68ba0d58a3
Javier Rodriguez Reina <javrodrei@alum.us.es>
2024-12-01 19:24:59 +0100

    fix: Remove unused files, imports and commets

--- app/modules/dashboard/repositories.py
+++ app/modules/dashboard/repositories.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 
 class DashboardRepository(BaseRepository):
     def __init__(self):
-        super().__init__(DataSet)  # Utilizamos DataSet en lugar de Notepad para contar los datasets
+        super().__init__(DataSet) 
 
     def get_dataset_count(self):
         return DataSet.query.count()
@@ -40,32 +40,22 @@ class DashboardRepository(BaseRepository):
         return result 
     
     def get_last_5_months_downloads(self):
-        # Obtener la fecha actual
         today = datetime.today()
-        
-        # Inicializamos las listas de meses y descargas
         months = []
         download_counts = []
-
-        # Iterar sobre los últimos 5 meses (mes actual + 4 anteriores)
         for i in range(5):
-            # Calcular el primer día de cada mes (mes actual, mes-1, ..., mes-4)
             first_day_of_month = today.replace(day=1) - timedelta(days=i * 30)
-            first_day_of_month_str = first_day_of_month.strftime('%Y-%m-01')  # Fecha en formato 'YYYY-MM-01'
-
-            # Consultar las descargas para ese mes
+            first_day_of_month_str = first_day_of_month.strftime('%Y-%m-01')  
             result = (
                 DSDownloadRecord.query
                 .filter(func.date(DSDownloadRecord.download_date) >= first_day_of_month_str)
                 .filter(func.date(DSDownloadRecord.download_date) < (first_day_of_month + timedelta(days=32)).strftime('%Y-%m-01'))
                 .count()
             )
-            
-            # Añadir el mes y el número de descargas a las listas
-            months.append(first_day_of_month.strftime('%Y-%m'))  # Añadimos el mes en formato 'YYYY-MM'
-            download_counts.append(result)  # Añadimos el número de descargas en ese mes
+            months.append(first_day_of_month.strftime('%Y-%m'))  
+            download_counts.append(result)  
 
-        # Invertir las listas para que las fechas más antiguas estén a la izquierda
+        
         months.reverse()
         download_counts.reverse()
 
--- app/modules/dashboard/routes.py
+++ app/modules/dashboard/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template
 from flask_login import login_required
-from app.modules.dashboard.forms import DashboardForm
 from app.modules.dashboard import dashboard_bp
 from app.modules.dashboard.services import DashboardService
 from app.modules.dataset.services import DataSetService
@@ -16,9 +15,6 @@ READ ALL
 @dashboard_bp.route('/dashboard', methods=['GET'])
 @login_required
 def index():
-    form = DashboardForm()
-    ndatasets = datasetservice.count_dsmetadata()
-    nauthors = datasetservice.count_authors()
 
     author_names, dataset_counts = dashboardService.get_all_author_names_and_dataset_counts()
     datasets_names, datasets_views = dashboardService.get_visits_per_dataset_lists()
@@ -28,11 +24,8 @@ def index():
     print(datasets_views)
 
     return render_template('dashboard/index.html', 
-                           ndatasets=ndatasets, 
-                           nauthors=nauthors,
                            author_names=author_names, 
                            datasets_count=dataset_counts, 
-                           form=form, 
                            datasets_names=datasets_names, 
                            datasets_views=datasets_views,
                            months=month,
