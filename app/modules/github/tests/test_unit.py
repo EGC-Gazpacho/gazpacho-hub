0d48c81083884fc412bd6f8bbd79ef754af04a7a
Rafael Duque <rafduqcol@alum.us.es>
2024-12-08 17:41:21 +0100

    fix: Fix some lint errros

--- app/modules/github/tests/test_unit.py
+++ app/modules/github/tests/test_unit.py
@@ -11,7 +11,6 @@ from app import db
 from app.modules.conftest import login, logout
 
 
-
 @pytest.fixture(scope="module")
 def test_client(test_client):
     with test_client.application.app_context():
@@ -28,22 +27,26 @@ def mock_dataset():
     return {"id": 1, "name": "Test Dataset", "files": ["file1.txt", "file2.txt"]}
 
 # Test the route get create_dataset_github
+
+
 def test_create_dataset_github(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get:
         mock_get.return_value = mock_dataset
-        
+
         response = test_client.get("/github/upload/1")
         assert response.status_code == 200
         logout(test_client)
 
 # Test the route create_dataset_github with success
+
+
 def test_create_dataset_github_succes(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get:
         mock_get.return_value = mock_dataset
 
@@ -68,7 +71,7 @@ def test_create_dataset_github_succes(test_client, mock_dataset):
 def test_create_new_repo(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get:
         mock_get.return_value = mock_dataset
 
@@ -85,7 +88,7 @@ def test_create_new_repo(test_client, mock_dataset):
                 })
 
                 assert response.status_code == 200
-                assert response.json["message"] == "Upload successful"  
+                assert response.json["message"] == "Upload successful"
                 logout(test_client)
 
 
@@ -93,7 +96,7 @@ def test_create_new_repo(test_client, mock_dataset):
 def test_repository_not_found(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get:
         mock_get.return_value = mock_dataset
 
@@ -112,12 +115,12 @@ def test_repository_not_found(test_client, mock_dataset):
             assert response.json["error"] == "Repository not found. Verify the repository owner and name."
             logout(test_client)
 
-          
+
 # Tests the route create_dataset_github with a branch that does not exist
 def test_branch_not_found(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get:
         mock_get.return_value = mock_dataset
 
@@ -138,10 +141,12 @@ def test_branch_not_found(test_client, mock_dataset):
                 logout(test_client)
 
 # Tests the route create_dataset_github with a connection error
+
+
 def test_upload_dataset_error(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get:
         mock_get.return_value = mock_dataset
 
@@ -162,11 +167,13 @@ def test_upload_dataset_error(test_client, mock_dataset):
                     assert response.json["error"] == "Failed to connect to GitHub API: Error de conexión"
                     logout(test_client)
 
-#Test the route create_dataset_github with a bad token
+# Test the route create_dataset_github with a bad token
+
+
 def test_bad_token(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get:
         mock_get.return_value = mock_dataset
 
@@ -186,32 +193,34 @@ def test_bad_token(test_client, mock_dataset):
                     assert response.status_code == 401
                     assert response.json["error"] == "Bad credentials. Verify your access token."
                     logout(test_client)
-                    
-#Test the route create_dataset_github with a dataset that already exists in the repository
+
+# Test the route create_dataset_github with a dataset that already exists in the repository
+
+
 def test_create_dataset_github_already_exists(test_client, mock_dataset):
     login_response = login(test_client, "user@example.com", "test1234")
     assert login_response.status_code == 200, "Login was unsuccessful."
-    
+
     with patch.object(GitHubService, 'check_repository_exists', return_value=True):
-            with patch.object(GitHubService, 'check_branch_exists', return_value=True):
-                with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get, \
+        with patch.object(GitHubService, 'check_branch_exists', return_value=True):
+            with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get, \
                     patch.object(GitHubService, 'upload_dataset_to_github') as mock_upload:
 
-                    mock_get.return_value = mock_dataset
+                mock_get.return_value = mock_dataset
 
-                    mock_upload.side_effect = requests.exceptions.HTTPError("422 Unprocessable Entity")
+                mock_upload.side_effect = requests.exceptions.HTTPError("422 Unprocessable Entity")
 
-                    response = test_client.post("/github/upload/1", data={
-                        'commit_message': 'Test commit',
-                        'owner': 'rafduqcol',
-                        'repo_name': 'uvl',
-                        'branch': 'main',
-                        'repo_type': 'existing',
-                        'access_token': os.getenv("GITHUB_TOKEN"),
-                        'license': 'MIT'
-                    })
-                    print(response.json)
-                    assert response.status_code == 422
-                    assert response.json["error"] == "A dataset with the same name already exists in the repository."
-                    assert response.json["code"] == 422
-                    logout(test_client)
+                response = test_client.post("/github/upload/1", data={
+                    'commit_message': 'Test commit',
+                    'owner': 'rafduqcol',
+                    'repo_name': 'uvl',
+                    'branch': 'main',
+                    'repo_type': 'existing',
+                    'access_token': os.getenv("GITHUB_TOKEN"),
+                    'license': 'MIT'
+                })
+                print(response.json)
+                assert response.status_code == 422
+                assert response.json["error"] == "A dataset with the same name already exists in the repository."
+                assert response.json["code"] == 422
+                logout(test_client)
