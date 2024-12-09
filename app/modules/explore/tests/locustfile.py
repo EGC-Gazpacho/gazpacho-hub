73b626ca37496c4971900f377d475715d9241559
andresmarevi <andresmarevi@gmail.com>
2024-12-09 17:04:59 +0100

    feat: Locust test developed

--- app/modules/explore/tests/locustfile.py
+++ app/modules/explore/tests/locustfile.py
@@ -1,15 +1,21 @@
-"""
 from locust import HttpUser, TaskSet, task
 from core.environment.host import get_host_for_locust_testing
 
-class ExploreBehavior(TaskSet):
-    @task
-    def explore_page(self):
-        self.client.get("/explore")
 
-class ExploreUser(HttpUser):
-    tasks = [ExploreBehavior]
+class ExploreModelsBehavior(TaskSet):
+    def on_start(self):
+        self.index()
+
+    @task
+    def index(self):
+        response = self.client.get("/explore.explore2_models")
+
+        if response.status_code != 200:
+            print(f"Explore models index failed: {response.status_code}")
+
+
+class ExploreModelsUser(HttpUser):
+    tasks = [ExploreModelsBehavior]
     min_wait = 5000
     max_wait = 9000
     host = get_host_for_locust_testing()
-"""
